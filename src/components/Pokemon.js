f8f32d36238deb84a40022ecfe957c0f8573f3ee
Kruxk <reinko.brink@gmail.com>
2020-04-24 16:43:05 +0200

    added pokemon component props

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,12 +4,11 @@ import React from "react";
 export default function Pokemon(props) {
   return (
     <div>
-      <h2>Pokemon name: {"Charizard"}</h2>
-      <p>Weight: {90} kg</p>
-      <p>Awesome: {true}</p>
-      <p>Terrifying: {false}</p>
-      <p>What about null? {null}</p>
-      <p>And undefined? {undefined}</p>
+      <h2>Pokemon name: {props.name}</h2>
+      <p>Weight: {props.weight} kg</p>
+      <p>Awesome: {props.awesome ? "YES!" : "nope, not really"}</p>
+      <p>Terrifying: {props.terrifying ? "Very" : "nah, lovable"}</p>
+      <p>Abilities: {props.abilities}</p>
     </div>
   );
 }
